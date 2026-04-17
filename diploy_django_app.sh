c1498cfada19c70c7ecb39de09d5f03f03f742ec
Ubuntu <ubuntu@ip-172-31-45-140.eu-north-1.compute.internal>
2026-04-17 03:20:03 +0000

    Django-files

---
+++ diploy_django_app.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+<<task
+Diploy dejango app and the handle the code for errors
+task
+
+#clonnig code
+
+code_clone() {
+
+	echo "Clonnig the fjango app"
+	git clone https://github.com/LondheShubham153/django-notes-app.git
+}
+
+install_requirements() {
+	
+	echo "Installing dependancies"
+	sudo apt update
+	sudo apt install docker.io nginx -y
+
+}
+
+required_restarts() {
+
+	sudo chmod 777 /var/run/docker.sock
+	sudo systemctl enable docker 
+	sudo systemctl enable nginx
+
+}
+
+deploy() {
+	
+
+	docker build -t notes-app . #creating docker image using docker file
+	docker run -dp 8000:8000 notes-app:latest # running the docker image
+
+}
+
+echo "-------------Deployment started----------------------"
+
+#Error handling
+if ! code_clone; then
+	echo "the code dir already exists"
+	cd django-notes-app
+fi
+
+if ! install_requirements; then 
+	echo "installation failed"
+	exit 1
+fi
+required_restarts
+deploy
+echo"---------Deployment done----------"
